bc9b1d5b70b43ea2b7822e1580a2bd8b9839e36f
Anthony G. Basile <basile@opensource.dyc.edu>
2011-05-06 21:44:26 -0400

    poc/mangle-paxflags.c: added verbose mode

--- poc/mangle-paxflags.c
+++ poc/mangle-paxflags.c
@@ -39,8 +39,10 @@
 #define HF_PAX_RANDEXEC         16	// 1: Randomize ET_EXEC base
 #define HF_PAX_SEGMEXEC         32	// 0: Segmentation based non-exec pages
 
-#define PRINT(E,F,I) printf("%s:\t%s\n", #E, E & F ? ( I ? "enabled" : "disabled" ) : ( I ? "disabled" : "enabled" ) );
-#define CASE(N,P) case P: printf("%d: %s\n", (int)N, #P); break
+#define PRINT(E,F,I)		printf("%s:\t%s\n", #E, E&F? (I? "enabled" : "disabled") : (I? "disabled" : "enabled"));
+#define SPRINT(E,F,A,B)		printf("%c", E&F? A : B);
+#define CPRINT(N,P)		case P: printf("%d: %s\n", (int)N, #P); break
+#define FPRINT(N,D,F,A,B)	printf("%c", N&F? (D&F? '*' : B) : (D&F? A : '-'))
 
 
 void
@@ -54,6 +56,7 @@ print_help(char *v)
                 "options      :     Print out EI_PAX and PT_PAX_FLAGS information\n"
                 "             : -e  Set all EI_PAX flags to least secure setting, pEmrXs\n"
                 "             : -p  Remove PT_PAX_FLAGS program header\n"
+		"             : -v  Verbose expanation of flags (rather than short list)\n"
                 "             : -h  Print out this help\n",
                 v
         );
@@ -63,16 +66,18 @@ print_help(char *v)
 
 
 char *
-parse_cmd_args( int c, char *v[], int *flag_ei_pax, int *flag_pt_pax_flags  )
+parse_cmd_args(int c, char *v[], int *flag_ei_pax, int *flag_pt_pax_flags, int *verbose)
 {
 	int i, oc;
 
 	if((c != 2)&&(c != 3)&&(c != 4))
-		error(EXIT_FAILURE, 0, "Usage: %s {[-e] [-p] ELFfile | [-h]}", v[0]);
+		error(EXIT_FAILURE, 0, "Usage: %s {[-e] [-p] [-v] ELFfile | [-h]}", v[0]);
 
 	*flag_ei_pax = 0;
 	*flag_pt_pax_flags = 0;
-	while((oc = getopt(c, v,":eph")) != -1)
+	*verbose = 0;
+
+	while((oc = getopt(c, v,":epvh")) != -1)
 		switch(oc)
 		{
 			case 'e':
@@ -80,6 +85,9 @@ parse_cmd_args( int c, char *v[], int *flag_ei_pax, int *flag_pt_pax_flags  )
 				break ;
 			case 'p':
 				*flag_pt_pax_flags = 1;
+				break;
+			case 'v':
+				*verbose = 1;
 				break;
 			case 'h':
 				print_help(v[0]);
@@ -96,8 +104,7 @@ parse_cmd_args( int c, char *v[], int *flag_ei_pax, int *flag_pt_pax_flags  )
 int
 main( int argc, char *argv[])
 {
-	int fd;
-	int flag_ei_pax, flag_pt_pax_flags, found_ei_pax;
+	int fd, flag_ei_pax, flag_pt_pax_flags, verbose, found_ei_pax;
 	char *f_name;
 	size_t i, phnum;
 
@@ -105,7 +112,7 @@ main( int argc, char *argv[])
 	GElf_Ehdr ehdr;
 	GElf_Phdr phdr;
 
-	f_name = parse_cmd_args(argc, argv, &flag_ei_pax, &flag_pt_pax_flags);
+	f_name = parse_cmd_args(argc, argv, &flag_ei_pax, &flag_pt_pax_flags, &verbose);
 
 	if(elf_version(EV_CURRENT) == EV_NONE)
 		error(EXIT_FAILURE, 0, "Library out of date.");
@@ -134,13 +141,26 @@ main( int argc, char *argv[])
 	found_ei_pax = ((u_long) ehdr.e_ident[EI_PAX + 1] << 8) + (u_long) ehdr.e_ident[EI_PAX];
 
 	printf("==== EI_PAX ====\n") ;
-	PRINT(HF_PAX_PAGEEXEC, found_ei_pax, 0);
-	PRINT(HF_PAX_EMUTRAMP, found_ei_pax, 1);
-	PRINT(HF_PAX_MPROTECT, found_ei_pax, 0);
-	PRINT(HF_PAX_RANDMMAP, found_ei_pax, 0);
-	PRINT(HF_PAX_RANDEXEC, found_ei_pax, 1);
-	PRINT(HF_PAX_SEGMEXEC, found_ei_pax, 0);
-	printf("\n");
+	if(verbose)
+	{
+		PRINT(HF_PAX_PAGEEXEC, found_ei_pax, 0);
+		PRINT(HF_PAX_EMUTRAMP, found_ei_pax, 1);
+		PRINT(HF_PAX_MPROTECT, found_ei_pax, 0);
+		PRINT(HF_PAX_RANDMMAP, found_ei_pax, 0);
+		PRINT(HF_PAX_RANDEXEC, found_ei_pax, 1);
+		PRINT(HF_PAX_SEGMEXEC, found_ei_pax, 0);
+		printf("\n");
+	}
+	else
+	{
+		SPRINT(HF_PAX_PAGEEXEC, found_ei_pax, 'p', 'P');
+		SPRINT(HF_PAX_EMUTRAMP, found_ei_pax, 'E', 'e');
+		SPRINT(HF_PAX_MPROTECT, found_ei_pax, 'm', 'M');
+		SPRINT(HF_PAX_RANDMMAP, found_ei_pax, 'r', 'R');
+		SPRINT(HF_PAX_RANDEXEC, found_ei_pax, 'X', 'x');
+		SPRINT(HF_PAX_SEGMEXEC, found_ei_pax, 's', 'S');
+		printf("\n\n");
+	}
 
 	if( flag_ei_pax )
 	{
@@ -158,45 +178,61 @@ main( int argc, char *argv[])
 		if(gelf_getphdr(elf, i, &phdr) != &phdr)
 			error(EXIT_FAILURE, 0, "gelf_getphdr(): %s", elf_errmsg(elf_errno()));
 
-		switch(phdr.p_type)
+		if(verbose)
 		{
-			CASE(i,PT_NULL);
-			CASE(i,PT_LOAD);
-			CASE(i,PT_DYNAMIC);
-			CASE(i,PT_INTERP);
-			CASE(i,PT_NOTE);
-			CASE(i,PT_SHLIB);
-			CASE(i,PT_PHDR);
-			CASE(i,PT_TLS);
-			CASE(i,PT_NUM);
-			CASE(i,PT_LOOS);
-			CASE(i,PT_GNU_EH_FRAME);
-			CASE(i,PT_GNU_STACK);
-			CASE(i,PT_GNU_RELRO);
-			CASE(i,PT_PAX_FLAGS);
-			CASE(i,PT_LOSUNW);
-			//CASE(i,PT_SUNWBSS);
-			CASE(i,PT_SUNWSTACK);
-			CASE(i,PT_HISUNW);
-			//CASE(i,PT_HIOS);
-			CASE(i,PT_LOPROC);
-			CASE(i,PT_HIPROC);
+			switch(phdr.p_type)
+			{
+				CPRINT(i,PT_NULL);
+				CPRINT(i,PT_LOAD);
+				CPRINT(i,PT_DYNAMIC);
+				CPRINT(i,PT_INTERP);
+				CPRINT(i,PT_NOTE);
+				CPRINT(i,PT_SHLIB);
+				CPRINT(i,PT_PHDR);
+				CPRINT(i,PT_TLS);
+				CPRINT(i,PT_NUM);
+				CPRINT(i,PT_LOOS);
+				CPRINT(i,PT_GNU_EH_FRAME);
+				CPRINT(i,PT_GNU_STACK);
+				CPRINT(i,PT_GNU_RELRO);
+				CPRINT(i,PT_PAX_FLAGS);
+				CPRINT(i,PT_LOSUNW);
+				//CPRINT(i,PT_SUNWBSS);
+				CPRINT(i,PT_SUNWSTACK);
+				CPRINT(i,PT_HISUNW);
+				//CPRINT(i,PT_HIOS);
+				CPRINT(i,PT_LOPROC);
+				CPRINT(i,PT_HIPROC);
+			}
 		}
 
 		if(phdr.p_type == PT_PAX_FLAGS)
 		{
-			PRINT(PF_PAGEEXEC, phdr.p_flags, 1);
-			PRINT(PF_NOPAGEEXEC, phdr.p_flags, 1);
-			PRINT(PF_SEGMEXEC, phdr.p_flags, 1);
-			PRINT(PF_NOSEGMEXEC, phdr.p_flags, 1);
-			PRINT(PF_MPROTECT, phdr.p_flags, 1);
-			PRINT(PF_NOMPROTECT, phdr.p_flags, 1);
-			PRINT(PF_RANDEXEC, phdr.p_flags, 1);
-			PRINT(PF_NORANDEXEC, phdr.p_flags, 1);
-			PRINT(PF_EMUTRAMP, phdr.p_flags, 1);
-			PRINT(PF_NOEMUTRAMP, phdr.p_flags, 1);
-			PRINT(PF_RANDMMAP, phdr.p_flags, 1);
-			PRINT(PF_NORANDMMAP, phdr.p_flags, 1);
+			if(verbose)
+			{
+				PRINT(PF_PAGEEXEC,   phdr.p_flags, 1);
+				PRINT(PF_NOPAGEEXEC, phdr.p_flags, 1);
+				PRINT(PF_SEGMEXEC,   phdr.p_flags, 1);
+				PRINT(PF_NOSEGMEXEC, phdr.p_flags, 1);
+				PRINT(PF_MPROTECT,   phdr.p_flags, 1);
+				PRINT(PF_NOMPROTECT, phdr.p_flags, 1);
+				PRINT(PF_RANDEXEC,   phdr.p_flags, 1);
+				PRINT(PF_NORANDEXEC, phdr.p_flags, 1);
+				PRINT(PF_EMUTRAMP,   phdr.p_flags, 1);
+				PRINT(PF_NOEMUTRAMP, phdr.p_flags, 1);
+				PRINT(PF_RANDMMAP,   phdr.p_flags, 1);
+				PRINT(PF_NORANDMMAP, phdr.p_flags, 1);
+			}
+			else
+			{
+				printf("%d: PT_PAX_FLAGS\n", (int)i);
+				FPRINT(PF_PAGEEXEC, PF_NOPAGEEXEC, phdr.p_flags, 'p', 'P');
+				FPRINT(PF_EMUTRAMP, PF_NOEMUTRAMP, phdr.p_flags, 'e', 'E');
+				FPRINT(PF_MPROTECT, PF_NOMPROTECT, phdr.p_flags, 'm', 'M');
+				FPRINT(PF_RANDMMAP, PF_NORANDMMAP, phdr.p_flags, 'r', 'R');
+				FPRINT(PF_RANDEXEC, PF_NORANDEXEC, phdr.p_flags, 'x', 'X');
+				FPRINT(PF_SEGMEXEC, PF_NOSEGMEXEC, phdr.p_flags, 's', 'S');
+			}
 		}
 
 		if((phdr.p_type == PT_PAX_FLAGS) && flag_pt_pax_flags )
