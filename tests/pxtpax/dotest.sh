42d211fc0b735d29c98fe91e0a2a69380d5dfaa3
Anthony G. Basile <blueness@gentoo.org>
2011-11-15 14:25:18 -0500

    Fix some bugs in tests/*

--- tests/pxtpax/dotest.sh
+++ tests/pxtpax/dotest.sh
@@ -4,9 +4,11 @@ PWD=$(pwd)
 INITSH="${PWD}"/init.sh
 DAEMON="${PWD}"/daemon
 PIDFILE="${PWD}"/daemon.pid
+PAXCTLNG="../../src/paxctl-ng"
 
-paxctl-ng -cv ${DAEMON}
+${PAXCTLNG} -cv ${DAEMON} 2>&1 1>/dev/null
 
+echo "xattr  process"
 for pf in "p" "P"; do
   for ef in "e" "E"; do
     for mf in "m" "M"; do
@@ -14,13 +16,15 @@ for pf in "p" "P"; do
         for sf in "s" "S"; do
           flags="${pf}${ef}${mf}${rf}${sf}"
           echo -n ${flags} " "
-          paxctl-ng -"${flags}" ${DAEMON} 2>&1 1>/dev/null
+          ${PAXCTLNG} -"${flags}" ${DAEMON} 2>&1 1>/dev/null
           ${INITSH} start
           if [ -f "${PIDFILE}" ]
           then
             rflags=$(cat /proc/$(cat ${PIDFILE})/status | grep ^PaX | awk '{ print $2 }')
             echo -n ${rflags}
             ${INITSH} stop
+          else
+            echo -n "no daemon"
           fi
           echo
         done
